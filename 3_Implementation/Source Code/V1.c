c66028bffde0bd0d04c2e1a314f20eabd42cd252
Sanjana G <101441389+SanjanaGireesh@users.noreply.github.com>
2022-04-22 19:06:21 +0530

    Update V1.c

--- 3_Implementation/Source Code/V1.c
+++ 3_Implementation/Source Code/V1.c
@@ -1,25 +1,3 @@
-/*******************************************************
-This program was created by the
-CodeWizardAVR V3.12 Advanced
-Automatic Program Generator
-© Copyright 1998-2014 Pavel Haiduc, HP InfoTech s.r.l.
-http://www.hpinfotech.com
-
-Project : 
-Version : 
-Date    : 19-04-2022
-Author  : 
-Company : 
-Comments: 
-
-
-Chip type               : ATmega328
-Program type            : Application
-AVR Core Clock frequency: 8.000000 MHz
-Memory model            : Small
-External RAM size       : 0
-Data Stack size         : 512
-*******************************************************/
 
 #include <mega328.h>
 
